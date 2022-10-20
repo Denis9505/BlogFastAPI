af979917e657dccd8a24a664e233cddf4ecb7edd
Denis9505 <victoria_l98@mail.ru>
2022-10-20 21:06:51 +0300

    Drop files

--- routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from api import blog, auth
-
-
-routes = APIRouter()
-
-routes.include_router(blog.router,prefix="/blog")
-routes.include_router(auth.router, prefix='/auth')
